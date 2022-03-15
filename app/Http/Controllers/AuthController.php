2ce561f916c4cdc30da4cf1605108e25da44ef74
0xSheff <0xsheff@gmail.com>
2022-03-15 17:33:55 +0200

    register and login added

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        $user = User::create(
+            $request->only('first_name', 'last_name', 'email')
+            + [
+                'password' => \Hash::make($request->input('password'))
+            ]
+        );
+
+        return response($user, Response::HTTP_CREATED);
+    }
+
+    public function login(Request $request)
+    {
+        if (!\Auth::attempt($request->only('email', 'password'))) {
+            return response([
+                'error' => 'invalid credentials'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $user = \Auth::user();
+
+        $jwt = $user->createToken('token')->plainTextToken;
+
+        $cookie = cookie('jwt', $jwt, 60*24*7); // 1 week
+
+        return response([
+            'message' => 'success'
+        ])->withCookie($cookie);
+    }
+
+}
