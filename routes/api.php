a9f80f26c464f1f402c157d825363c17203f4b9c
0xSheff <0xsheff@gmail.com>
2022-03-16 12:17:39 +0200

    seeders and user scopes added

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AmbassadorController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -25,6 +26,8 @@ Route::prefix('admin')->group(function () {
         Route::post('logout', [AuthController::class, 'logout']);
         Route::put('user/update-info', [AuthController::class, 'updateInfo']);
         Route::put('users/password', [AuthController::class, 'updatePassword']);
+
+        Route::get('ambassadors', [AmbassadorController::class, 'index']);
     });
 
 });
