235fb76d174cbab80bd1c27850aa04aa2c56ecd7
0xSheff <0xsheff@gmail.com>
2022-03-18 17:02:41 +0200

    orders processing with Stripe payments

--- routes/api.php
+++ routes/api.php
@@ -66,4 +66,6 @@ Route::prefix('ambassador')->group(function () {
 // Checkout
 Route::prefix('checkout')->group(function () {
     Route::get('links/{code}', [LinkController::class, 'show']);
+    Route::post('orders', [OrderController::class, 'store']);
+    Route::post('orders/confirm', [OrderController::class, 'confirm']);
 });
